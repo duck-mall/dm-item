964990ebef2a06503dd109b12e2c4604a162aea4
1549063028@qq.com <1549063028@qq.com>
2022-08-16 20:59:09 +0800

    refactor(bootstrap):包名拼写调整

--- src/main/java/com/duckmall/dm_item/DmItemApplication.java
+++ src/main/java/com/duckmall/dm_item/DmItemApplication.java
@@ -1,4 +1,4 @@
-package com.ducumall.dm_item;
+package com.duckmall.dm_item;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/duckmall/dm_item/DmItemApplicationTests.java
+++ src/test/java/com/duckmall/dm_item/DmItemApplicationTests.java
@@ -1,4 +1,4 @@
-package com.ducumall.dm_item;
+package com.duckmall.dm_item;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
