e0ba00369d719c857b75a5252d4251e0365b311b
frank <1823427924@qq.com>
2022-08-16 16:09:07 +0800

    init

---
+++ src/main/java/com/ducumall/dm_item/DmItemApplication.java
@@ -0,0 +1,13 @@
+package com.ducumall.dm_item;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DmItemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DmItemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ducumall/dm_item/DmItemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ducumall.dm_item;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DmItemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
