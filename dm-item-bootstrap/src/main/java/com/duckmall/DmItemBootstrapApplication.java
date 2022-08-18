4480e8a272d9a0798f43298e0fac499ef316691f
xiong <1549063028@qq.com>
2022-08-18 23:26:37 +0800

    feat(bootstrap):启动成功打印连接信息

--- dm-item-bootstrap/src/main/java/com/duckmall/DmItemBootstrapApplication.java
+++ dm-item-bootstrap/src/main/java/com/duckmall/DmItemBootstrapApplication.java
@@ -3,13 +3,31 @@ package com.duckmall;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 public class DmItemBootstrapApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DmItemBootstrapApplication.class, args);
+	public static void main(String[] args) throws UnknownHostException {
+		ConfigurableApplicationContext application = SpringApplication.run(DmItemBootstrapApplication.class, args);
+		Environment env = application.getEnvironment();
+		String ip = InetAddress.getLocalHost().getHostAddress();
+		String port = env.getProperty("server.port");
+		String property = env.getProperty("server.servlet.context-path");
+		String path = property == null ? "" :  property;
+		System.out.println(
+				"\n\t" +
+						"----------------------------------------------------------\n\t" +
+						"Application im-item is running! Access URLs:\n\t" +
+						"Local: \t\thttp://localhost:" + port + path + "/\n\t" +
+						"External: \thttp://" + ip + ":" + port + path + "/\n\t" +
+						"------------------------------------------------------------");
+
 	}
 
 }
